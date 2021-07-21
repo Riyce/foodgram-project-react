2489497dc129ab1d65e52e54a07eedb88d73b79c
kuzminov.f <riyce@mail.ru>
2021-07-21 10:46:37 +0300

    fix pur button

--- recipes/templatetags/recipe_filters.py
+++ recipes/templatetags/recipe_filters.py
@@ -11,3 +11,8 @@ def favorite_by_user(recipe, user):
 @register.filter
 def get_purchases_count(user):
     return user.purchases.all().count()
+
+
+@register.filter
+def add_to_user_purchases(recipe, user):
+    return user.purchases.filter(recipe=recipe).exists()
