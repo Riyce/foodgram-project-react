b864b8828cf4b23e71933ca0538a823ae4156c8d
kuzminov.f <riyce@mail.ru>
2021-07-23 01:06:44 +0300

    try

--- recipes/utils.py
+++ recipes/utils.py
@@ -25,6 +25,8 @@ def get_ingredients(request):
                 ingredients[ing] += ing_count
                 continue
             ingredients[ing] = ing_count
+    if not ingredients:
+        raise ValidationError('Вы ничего не добавили.')
     return ingredients
 
 
